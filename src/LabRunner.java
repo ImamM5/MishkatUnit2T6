7fe73662db7837fc3c065e751261a050eba3a427
ImamM5 <imamm@nycstudents.net>
2022-10-11 09:27:29 -0400

    new code

--- src/LabRunner.java
+++ src/LabRunner.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class LabRunner {
+public class LabRunner
+{
+    public static void main(String[] args)
+    {
+
+        System.out.println("My cat says "meow" when she is happy!");
+
+
+    }
 }
